f4e92d38ec76cc6baedfeafbec2d3f7200cde38d
E.  Cheynet <ECheynet@users.noreply.github.com>
2022-02-08 14:09:51 +0100

    Add files via upload

--- windsurfer.m
+++ windsurfer.m
@@ -1,6 +1,6 @@
 
 function [data] = windsurfer(targetLat,targetLon,targetYear,targetMonth,...
-    targetDay,targetHour,output,resolution)
+    targetDay,output,resolution,varargin)
 %
 % [data] = windsurfer(targetLat,targetLon,targetYear,targetMonth,...
 %     targetDay,targetHour,output,resolution)
@@ -60,31 +60,62 @@ function [data] = windsurfer(targetLat,targetLon,targetYear,targetMonth,...
 %
 % Author: E. Cheynet - UiB, Norway - last modified: 06-12-2021
 
+%% Optional aprameters
+
+
+p = inputParser();
+p.CaseSensitive = false;
+p.addOptional('newLon',[]); % optional parameters -- longitude vector
+p.addOptional('newLat',[]); % optional parameters -- latitude vector
+p.parse(varargin{:});
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+newLat = p.Results.newLat ;
+newLon = p.Results.newLon ;
+
 %% Definition of the grid
-[lat,lon] = meshgrid(targetLat(1):resolution:targetLat(end),targetLon(1):resolution:targetLon(end));
-%% Check month and day number + transform date into string
-if targetMonth<10,    myMonth = ['0',num2str(targetMonth)];else    myMonth = num2str(targetMonth);end
-if targetDay<10,    myDay = ['0',num2str(targetDay)];else    myDay = num2str(targetDay);end
-if targetHour<10,    myHour = ['0',num2str(targetHour)];else    myHour = num2str(targetHour);end
-myYear = num2str(targetYear);
+if ~isempty(newLon) && ~isempty(newLat)
+    lon = newLon;
+    lat = newLat;
+    Nlat = numel(lat);
+else
+    [lon,lat] = meshgrid(targetLon(1):resolution:targetLon(end),targetLat(1):resolution:targetLat(end));
+    [Nlat,Nlon]=size(lat);
+end
+
+if numel(targetDay)==1, targetDay = ['0',targetDay];end
+if numel(targetMonth)==1, targetMonth = ['0',targetMonth];end
+% if numel(targetHour)==1, targetHour = ['0',targetHour];end
 %% Preallocation and initalisation
 data = struct('time',[],'lon',[],'lat',[],'ff',[],'dd',[],'fv',[],'dc',[],'hs',[],'tp',[],...
     'hs_sea',[],'tp_sea',[],'hs_swell',[],'tp_swell',[],...
     'thq',[],'thq_sea',[],'thq_swell',[],'sic',[],'sit',[],'model_depth',[]);
-
 urldat= ['https://thredds.met.no/thredds/dodsC/windsurfer/mywavewam3km_files/',...
-    myYear,'/',myMonth,'/',myYear,myMonth,myDay,'_MyWam3km_hindcast.nc'];
+    targetYear,'/',targetMonth,'/',targetYear,targetMonth,targetDay,'_MyWam3km_hindcast.nc'];
 time0 = ncread(urldat,'time')./86400+datenum('1970-01-01 00:00:00');
 time = datetime(datestr(double(time0)));
-[~,indTime]=min(abs(hour(time)-str2double(targetHour)));
-data.time = time(indTime);
-
+% [~,indTime]=min(abs(hour(time)-str2double(targetHour)));
+data.time = time;
 %% Get narrower area to speed up the reading procedure
 lon00 = ncread(urldat,'longitude');
 lat00 = ncread(urldat,'latitude');
-offset = 0.5*sqrt(diff(targetLat).^2 + diff(targetLon).^2);
-[~,indStart] = min(sqrt((lat00(:)-(targetLat(1)-offset)).^2 + abs(lon00(:)-(targetLon(2)-offset)).^2));
-[~,indEnd] = min(sqrt((lat00(:)-(targetLat(2)+offset)).^2 + abs(lon00(:)-(targetLon(1)+offset)).^2));
+
+
+if numel(targetLat)==1 &&   numel(targetLon)==1
+    offsetLat = 0.3;
+    offsetLon = 0.15;
+    [~,indStart] = min(sqrt((lat00(:)-(targetLat(1)-offsetLat)).^2 + abs(lon00(:)-(targetLon(1)-offsetLon)).^2));
+    [~,indEnd] = min(sqrt((lat00(:)-(targetLat(1)+offsetLat)).^2 + abs(lon00(:)-(targetLon(1)+offsetLon)).^2));
+elseif numel(targetLat)==2 &&   numel(targetLat)==2
+    
+    offsetLat = 0.5*sqrt(diff(targetLat).^2 + diff(targetLon).^2);
+    offsetLon = offsetLat;
+    [~,indStart] = min(sqrt((lat00(:)-(targetLat(1)-offsetLat)).^2 + abs(lon00(:)-(targetLon(2)-offsetLon)).^2));
+    [~,indEnd] = min(sqrt((lat00(:)-(targetLat(2)+offsetLat)).^2 + abs(lon00(:)-(targetLon(1)+offsetLon)).^2));
+else
+    error('targetLat and targetLatmust have the same dimensions')
+end
+
+
 [row1, col1] = ind2sub(size(lat00), indStart);
 [row2, col2] = ind2sub(size(lat00), indEnd);
 r1 = min(row1,row2); % row start
@@ -95,38 +126,78 @@ lon00 = ncread(urldat,'longitude',[r1,c1],[cr,cc]);
 lat00 = ncread(urldat,'latitude',[r1,c1],[cr,cc]);
 dummyLat = double(lat00(:));
 dummyLon = double(lon00(:));
-ind = find(dummyLat>=min(targetLat(:)-offset) & dummyLat <= max(targetLat(:)+offset) &...
-    dummyLon>=min(targetLon(:)-offset) & dummyLon <= max(targetLon(:)+offset));
+ind = find(dummyLat>=min(targetLat(:)-offsetLat) & dummyLat <= max(targetLat(:)+offsetLat) &...
+    dummyLon>=min(targetLon(:)-offsetLon) & dummyLon <= max(targetLon(:)+offsetLon));
+
+
 %% Read the data in a for loop for each selected output
 %  Data are resampled spatially as gridded data
+
+    
 Nout = numel(output);
-for ii=1:Nout
-    if strcmpi(output{ii},'model_depth'),
-        myVar0 = ncread(urldat,output{ii},[r1,c1],[cr,cc]);
-    else
-        myVar0 = ncread(urldat,output{ii},[r1,c1,indTime],[cr,cc,1]);
+Nhour = 24; % number of hours for 1 day
+
+if ~isempty(newLat),
+    for ii=1:Nout
+        if strcmpi(output{ii},'model_depth'),
+            myVar0 = ncread(urldat,output{ii},[r1,c1],[cr,cc]);
+            dummyVar = double(reshape(myVar0,[],1));
+            F_Var = scatteredInterpolant(dummyLat(ind),dummyLon(ind),dummyVar(ind),'linear','none');
+            data.(lower(output{ii}))(jj,:) = F_Var(lat,lon);
+        else
+            myVar0 = ncread(urldat,output{ii},[r1,c1,1],[cr,cc,Nhour]);
+            
+            
+            for jj=1:Nhour
+                dummyVar = double(reshape(myVar0(:,:,jj),[],1));
+                if strcmpi(output{ii},'dd') || contains(output{ii},'thq')
+                    un = cosd(dummyVar);
+                    ue = sind(dummyVar);
+                    F_ue = scatteredInterpolant(dummyLat(ind),dummyLon(ind),un(ind),'linear','none');
+                    F_un = scatteredInterpolant(dummyLat(ind),dummyLon(ind),ue(ind),'linear','none');
+                    newUe = F_ue(lat,lon);
+                    newUn = F_un(lat,lon);
+                    newDir = atan2(newUn,newUe).*180/pi;
+                    newDir(newDir<=0) = newDir(newDir<=0)+ 360;
+                    data.(lower(output{ii}))(jj,:) = newDir;
+                else
+                    F_Var = scatteredInterpolant(dummyLat(ind),dummyLon(ind),dummyVar(ind),'linear','none');
+                    data.(lower(output{ii}))(jj,:) = F_Var(lat,lon);
+                end
+            end
+        end
     end
-    dummyVar = double(myVar0(:));
-    if strcmpi(output{ii},'dd') || contains(output{ii},'thq')
-        
-        un = cosd(dummyVar);
-        ue = sind(dummyVar);
-        F_ue = scatteredInterpolant(dummyLat(ind),dummyLon(ind),un(ind),'linear','none');
-        F_un = scatteredInterpolant(dummyLat(ind),dummyLon(ind),ue(ind),'linear','none');
-        newUe = F_ue(lat,lon);
-        newUn = F_un(lat,lon);
-        newDir = atan2(newUn,newUe).*180/pi;
-        newDir(newDir<=0) = newDir(newDir<=0)+ 360;
-        data.(lower(output{ii})) = newDir;
-        
-        
-    else
-        
-        F_Var = scatteredInterpolant(dummyLat(ind),dummyLon(ind),dummyVar(ind),'linear','none');
-        data.(lower(output{ii})) = F_Var(lat,lon);
+else
+    for ii=1:Nout
+        if strcmpi(output{ii},'model_depth'),
+            myVar0 = ncread(urldat,output{ii},[r1,c1],[cr,cc]);
+            dummyVar = double(reshape(myVar0,[],1));
+            F_Var = scatteredInterpolant(dummyLat(ind),dummyLon(ind),dummyVar(ind),'linear','none');
+            data.(lower(output{ii}))(jj,:,:) = F_Var(lat,lon);
+        else
+            myVar0 = ncread(urldat,output{ii},[r1,c1,1],[cr,cc,Nhour]);
+            
+            
+            for jj=1:Nhour
+                dummyVar = double(reshape(myVar0(:,:,jj),[],1));
+                if strcmpi(output{ii},'dd') || contains(output{ii},'thq')
+                    un = cosd(dummyVar);
+                    ue = sind(dummyVar);
+                    F_ue = scatteredInterpolant(dummyLat(ind),dummyLon(ind),un(ind),'linear','none');
+                    F_un = scatteredInterpolant(dummyLat(ind),dummyLon(ind),ue(ind),'linear','none');
+                    newUe = F_ue(lat,lon);
+                    newUn = F_un(lat,lon);
+                    newDir = atan2(newUn,newUe).*180/pi;
+                    newDir(newDir<=0) = newDir(newDir<=0)+ 360;
+                    data.(lower(output{ii}))(jj,:,:) = newDir;
+                else
+                    F_Var = scatteredInterpolant(dummyLat(ind),dummyLon(ind),dummyVar(ind),'linear','none');
+                    data.(lower(output{ii}))(jj,:,:) = F_Var(lat,lon);
+                end
+            end
+        end
     end
 end
-
 data.lon = lon;
 data.lat = lat;
 
